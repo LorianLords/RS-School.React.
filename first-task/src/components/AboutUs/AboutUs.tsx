db5c1ddd414b14ffcd91fc2470837c4110d9495d
Dadayev <dadayevkerim@gmail.com>
2023-03-24 11:07:22 +0300

    The logic of saving input values in LocalStorage during component unmounting was implemented

--- first-task/src/components/AboutUs/AboutUs.tsx
+++ first-task/src/components/AboutUs/AboutUs.tsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, {Component} from "react";
 import s from "./AboutUs.module.css"
-const AboutUs = () => {
-    return(
-        <div>
-           <h1 className={s.text}>Hello and thanks for coming</h1>
-        </div>
-    )
+class AboutUs extends Component<any, any> {
+    render() {
+        return (
+            <div>
+                <h1 className={s.text}>Hello and thanks for coming</h1>
+            </div>
+        )
+    }
 }
 
 export default AboutUs;
